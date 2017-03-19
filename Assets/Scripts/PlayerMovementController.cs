dba5d5c26be2685b6948e80596852eaa2c44f2ae
Betekhtin <st040186@student.spbu.ru>
2017-03-19 15:41:04 +0300

    Joystick Movement Controller

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovementController : MonoBehaviour {
+
+	//Rigidbody2D playerBody;
+	public float speedo;
+
+	void Start () {
+		//playerBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+		Vector3 movement = new Vector3 (CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"), 0);
+		transform.position += movement * speedo;
+         
+	}
+}
