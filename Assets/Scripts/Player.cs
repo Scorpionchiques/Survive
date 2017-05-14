8d03d429f7292e4d4b9ca44fb40e4425a4e3c421
inventive-seven <mr.kakorin@yandex.ru>
2017-05-14 05:31:53 +0300

    same as prev

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,26 +5,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
     public float speedo;
     Rigidbody2D rigidBody_player;
     private Animator animator_player;
+
     PlayerMovementController movement;
+    AnimatorController player_animation;
     InventoryController inventory;
 
-    //public Event UseItem(Item);
-
     Item itemInHands;
+
+    public delegate void ResponseFromObject(Item i, Player p);
+
+    public event ResponseFromObject ClickOn;
 
     void Start()
     {
         rigidBody_player = GetComponent<Rigidbody2D>();
         animator_player = GetComponent<Animator>();
         movement = new PlayerMovementController();
+        player_animation = new AnimatorController();
         inventory = new InventoryController();
+        itemInHands = null;
     }
-
+    
     void FixedUpdate()
     {
         movement.move(rigidBody_player, animator_player, speedo);
+        player_animation.move(animator_player);
+    }
+   
+    private void OnMouseDown()
+    {
+        ClickOn(itemInHands, this);
+    }
+    public void ClickedObject()
+    {
+        ClickOn(itemInHands, this);
     }
 }
