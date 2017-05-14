bcd1145e64dc765d64426d3b59b83ae614488bda
Betekhtin <st040186@student.spbu.ru>
2017-05-14 12:17:55 +0300

    Statbars

---
+++ Assets/Scripts/BarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarController {
+
+    private GameObject bar;
+    private Image content;
+
+    public BarController(GameObject bar)
+    {
+        this.bar = bar;
+        this.content = bar.transform.GetChild(0).GetComponentInChildren<Image>();
+    }
+    
+    public void handleBar(float value, float maxValue)
+    {
+        if (value > maxValue) throw new Exception("Invalid stat value: " + bar.name);
+        content.fillAmount = value / maxValue;
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float health;
+    [SerializeField]
+    private float maxHealth;
+    [SerializeField]
+    private float stamina;
+    [SerializeField]
+    private float maxStamina;
 
     public float speedo;
+    public GameObject healthBar;
+    public GameObject staminaBar;
+
     Rigidbody2D rigidBody_player;
     private Animator animator_player;
 
     PlayerMovementController movement;
     AnimatorController player_animation;
     InventoryController inventory;
+
+    BarController healthBarControl;
+    BarController staminaBarControl; 
 
     Item itemInHands;
 
@@ -28,12 +42,16 @@ public class Player : MonoBehaviour
         player_animation = new AnimatorController();
         inventory = new InventoryController();
         itemInHands = null;
+        healthBarControl = new BarController(healthBar);
+        staminaBarControl = new BarController(staminaBar);
     }
     
     void FixedUpdate()
     {
         movement.move(rigidBody_player, animator_player, speedo);
         player_animation.move(animator_player);
+        healthBarControl.handleBar(health, maxHealth);
+        staminaBarControl.handleBar(stamina, maxStamina);
     }
    
     private void OnMouseDown()
